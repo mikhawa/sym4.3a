e90ab6fca904c3e68370abdec83ea20c627a72fa
mikhawa <michaeljpitz@gmail.com>
2019-08-01 00:07:57 +0200

    php bin/console make:fixtures ans load Post in PostFixtures.php

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -4,13 +4,16 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Entity\Post;
 
 class PostFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
+        $post=new Post();
         // $manager->persist($product);
+        $manager->persist($post);
 
         $manager->flush();
     }

aed0d0a947f1ed7110611e8a01b102090866ebd2
mikhawa <michaeljpitz@gmail.com>
2019-08-06 23:02:38 +0200

    RouterInterface and generate url

--- README.md
+++ README.md
@@ -133,3 +133,20 @@ create SecondController
 
         php bin/console make:controller
 
+for generate url into Controller:
+
+        use Symfony\Component\Routing\RouterInterface;
+        ...
+        /**
+        * @Route("/second/{page}", name="second", requirements={"page" = "\d+"}, defaults={"page" = 1})
+        */
+        public function index(RouterInterface $router)
+        {
+            $url = $router->generate(
+            'second', // 1er argument : le nom de la route
+            ['page' => mt_rand(1,30)]       // 2e argument : les paramètres
+        );
+        return $this->render('second/index.html.twig', [
+            'page' => $url,
+        ]);
+        }
--- src/Controller/SecondController.php
+++ src/Controller/SecondController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 class SecondController extends AbstractController
 {
     /**
-     * @Route("/second/", name="second")
+     * @Route("/second/{page}", name="second", requirements={"page" = "\d+"}, defaults={"page" = 1})
      */
-    public function index()
+    public function index(RouterInterface $router)
     {
+        $url = $router->generate(
+        'second', // 1er argument : le nom de la route
+        ['page' => mt_rand(1,30)]       // 2e argument : les paramètres
+    );
         return $this->render('second/index.html.twig', [
-            'controller_name' => 'SecondController',
+            'page' => $url,
         ]);
     }
 }

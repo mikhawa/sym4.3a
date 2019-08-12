bfd92ea24fb386a9c3277e8e4c38b9ecc138238e
mikhawa <michaeljpitz@gmail.com>
2019-08-12 13:31:07 +0200

    change fixtures for Lorem Ipsum generator

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -12,17 +12,62 @@ class PostFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
-        for ($i = 21; $i < 50; $i++) {
+        for ($i = 0; $i < 29; $i++) {
             $post=new Post();
-            $newTitle = 'Product The Norme '.$i;
+            $newTitle = $this->getLorem()[$i];
             $post->setTitle($newTitle);
             $post->setSlug(Slugger::slugify($newTitle));
-            $post->setContent(mt_rand(10, 1000));
+            $post->setContent($this->getRandomText(random_int(255, 1024)));
             $post->setAuthorEmail("michael.pitz@cf2m.be");
             $manager->persist($post);
         }
 
 
         $manager->flush();
+    }
+    // for Lorem Ipsum
+    private function getLorem(): array
+    {
+        return [
+            'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+            'Pellentesque vitae velit ex',
+            'Mauris dapibus risus quis suscipit vulputate',
+            'Eros diam egestas libero eu vulputate risus',
+            'In hac habitasse platea dictumst',
+            'Morbi tempus commodo mattis',
+            'Ut suscipit posuere justo at vulputate',
+            'Ut eleifend mauris et risus ultrices egestas',
+            'Aliquam sodales odio id eleifend tristique',
+            'Urna nisl sollicitudin id varius orci quam id turpis',
+            'Nulla porta lobortis ligula vel egestas',
+            'Curabitur aliquam euismod dolor non ornare',
+            'Sed varius a risus eget aliquam',
+            'Nunc viverra elit ac laoreet suscipit',
+            'Pellentesque et sapien pulvinar consectetur',
+            'Ubi est barbatus nix',
+            'Abnobas sunt hilotaes de placidus vita',
+            'Ubi est audax amicitia',
+            'Eposs sunt solems de superbus fortis',
+            'Vae humani generis',
+            'Diatrias tolerare tanquam noster caesium',
+            'Teres talis saepe tractare de camerarius flavum sensorem',
+            'Silva de secundus galatae demitto quadra',
+            'Sunt accentores vitare salvus flavum parses',
+            'Potus sensim ad ferox abnoba',
+            'Sunt seculaes transferre talis camerarius fluctuies',
+            'Era brevis ratione est',
+            'Sunt torquises imitari velox mirabilis medicinaes',
+            'Mineralis persuadere omnes finises desiderium',
+            'Bassus fatalis classiss virtualiter transferre de flavum',
+        ];
+    }
+    private function getRandomText(int $maxLength = 255): string
+    {
+        $phrases = $this->getLorem();
+        shuffle($phrases);
+        while (mb_strlen($text = implode('. ', $phrases).'.') > $maxLength) {
+            array_pop($phrases);
+        }
+        return $text;
     }
 }
